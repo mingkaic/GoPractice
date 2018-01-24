4091ec6d53817791d98a468acffdc719fa82d61d
mingkaic <mchen@infoblox.com>
2018-01-24 09:30:43 -0800

    removed misplaced i < 10 conditional in EncodeToks

--- Practice1/main.go
+++ Practice1/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/mingkaic/Practice/Practice1/putil"
+	"github.com/mingkaic/GoPractice/Practice1/putil"
 )
 
 func main() {
@@ -14,8 +14,8 @@ func main() {
 	if len(args) != 2 {
 		log.Fatalf("insufficient arguments: require 2")
 	}
-	var base int64 = putil.StrConv([]byte(args[0]))
-	var b10 int64 = putil.StrConv([]byte(args[1]))
+	base := putil.StrConv([]byte(args[0]))
+	b10 := putil.StrConv([]byte(args[1]))
 	result := putil.BaseConv(base, b10)
 	fmt.Println(putil.EncodeToks(result))
 }
--- Practice1/putil/putil.go
+++ Practice1/putil/putil.go
@@ -1,17 +1,12 @@
 package putil
 
-func reverse(arr []uint8) {
-	n := len(arr)
-	for i := 0; i < n/2; i++ {
-		buffer := arr[i]
-		arr[i] = arr[n-i-1]
-		arr[n-i-1] = buffer
-	}
-}
+var encToks = serial('0', 10) + serial('a', 26)
 
-// convert baseStr to base-10 representation
+// ======== Public ========
+
+// StrConv ... convert baseStr to base-10 representation
 func StrConv(baseStr []byte) int64 {
-	var result int64 = 0
+	var result int64
 	var mul int64 = 1
 	reverse(baseStr)
 	for _, baseChar := range baseStr {
@@ -21,32 +16,44 @@ func StrConv(baseStr []byte) int64 {
 	return result
 }
 
+// BaseConv ... convert b10 base 10 value to base
 func BaseConv(base, b10 int64) []uint8 {
 	var accum []uint8
 	for b10 > 0 {
-		accum = append(accum, uint8(b10%base))
+		tok := uint8(b10 % base)
+		accum = append(accum, tok)
 		b10 /= base
 	}
 	reverse([]byte(accum))
 	return accum
 }
 
+// EncodeToks ... convert base-n value to its string representation
+func EncodeToks(tokens []uint8) string {
+	output := make([]byte, len(tokens))
+	for i, tok := range tokens {
+		output[i] = encToks[tok]
+	}
+	return string(output)
+}
+
+// ======== Private ========
+
+// reverse byte string in place
+func reverse(arr []byte) {
+	n := len(arr)
+	for i := 0; i < n/2; i++ {
+		buffer := arr[i]
+		arr[i] = arr[n-i-1]
+		arr[n-i-1] = buffer
+	}
+}
+
+// return a string starting with byte string from start to start + n - 1
 func serial(start byte, n uint8) string {
 	result := make([]byte, n)
 	for i := range result {
 		result[i] = start + byte(i)
 	}
 	return string(result)
-}
-
-var encToks = serial('0', 10) + serial('a', 26)
-
-func EncodeToks(tokens []uint8) string {
-	output := make([]byte, len(tokens))
-	for i, tok := range tokens {
-		if i < 10 {
-			output[i] = encToks[tok]
-		}
-	}
-	return string(output)
 }
