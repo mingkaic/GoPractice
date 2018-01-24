fc8658a1e40eb972c2a4d9b91c9ae923d6e1a778
mingkaic <mchen@infoblox.com>
2018-01-23 17:55:54 -0800

    reinterpret Practice 2 + add comments

--- Practice2/main.go
+++ Practice2/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"sync"
 
-	"github.com/mingkaic/Practice/Practice1/putil"
+	"github.com/mingkaic/GoPractice/Practice1/putil"
 )
 
 func main() {
@@ -16,22 +16,31 @@ func main() {
 		log.Fatalf("insufficient arguments: require 3")
 	}
 	var wg sync.WaitGroup
+	var mutex = &sync.Mutex{}
 	maxbase := putil.StrConv([]byte(args[0]))
 	b10 := putil.StrConv([]byte(args[1]))
-	nStart := int(putil.StrConv([]byte(args[2])))
-	if nStart < 2 {
-		nStart = 2
-	}
-	wg.Add(int(maxbase) - nStart)
-	var outstr string
-	for i := nStart; i < int(maxbase); i++ {
-		go func(wg *sync.WaitGroup, base, b10 int64) {
+	nPerStr := int(putil.StrConv([]byte(args[2])))
+	nOut := int(maxbase) - 1
+	wg.Add(nOut)
+	strBuffer := make([]string, nOut)
+	strIdx := 0
+	for i := 2; i <= int(maxbase); i++ {
+		go func(base, b10 int64) {
+			defer wg.Done()
 			result := putil.BaseConv(base, b10)
-			outstr += putil.EncodeToks(result) + ","
-			wg.Done()
-		}(&wg, int64(i), b10)
+			localstr := putil.EncodeToks(result)
+			mutex.Lock()
+			strBuffer[strIdx] = localstr
+			strIdx++
+			mutex.Unlock()
+		}(int64(i), b10)
 	}
 	wg.Wait()
-	fmt.Println(outstr)
-	fmt.Println(putil.EncodeToks(putil.BaseConv(maxbase, b10)))
+	for i, str := range strBuffer {
+		if i%nPerStr == nPerStr-1 || i == nOut-1 {
+			fmt.Println(str)
+		} else {
+			fmt.Print(str + ",")
+		}
+	}
 }
